d1af5cd819b70b904dd18f11cb945de6f7e9aff4
ronit-1404 <ronitdhase@gmail.com>
2026-06-16 17:17:59 +0530

    added genrative ai layer

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ import fs from 'fs';
 import { Queue } from "bullmq";
 import { QdrantVectorStore } from "@langchain/qdrant";
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const client = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const queue = new Queue("fileuploadqueue", {
     connection: {
         host: 'localhost',
@@ -59,13 +61,31 @@ app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
 app.get('/chat', async (req, res) => {
     try {
         const userquery = 'government'; // Hardcoded for now based on your code
-        
+
         const retriever = vectorstore.asRetriever({
             k: 2,
         });
-        
+
         const result = await retriever.invoke(userquery);
-        return res.json({ result });
+
+        const system_prompt = `
+        you are helpfull ai assistant who answers the user query based on the available context from pdf file.
+        Context:
+        ${JSON.stringify(result)}
+        `;
+
+        const model = client.getGenerativeModel({
+            model: "gemini-2.5-flash",
+            systemInstruction: system_prompt,
+        });
+
+        const chatresult = await model.generateContent(userquery);
+        const text = chatresult.response.text();
+
+        return res.json({
+            message: text,
+            docs: result,
+        });
     } catch (error) {
         console.error("Error in /chat:", error);
         return res.status(500).json({ error: error.message });
